b40c3957df379b95eabaedd607fde28b4ee47848
Harsh chaudhary <73737028+mrharsh0606@users.noreply.github.com>
2021-01-28 11:51:53 +0530

    Add files via upload

---
+++ binarytodeci.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int binaryTodecimal(int n)
+{
+    cout<<"enter a binary number";
+    cin>>n;
+    int x=1;
+    int ans=0;
+    while(n>0){
+       int y=n%10;
+       ans+=x*y;
+       x*=2;
+       n/=10;
+
+    }
+    return ans;
+    }
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<binaryTodecimal(n);
+
+}
